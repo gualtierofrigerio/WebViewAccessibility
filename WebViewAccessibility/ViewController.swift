9c59d06901135d682eb75d1b179c3f64d80127fe
Gualtiero Frigerio <gualtiero.frigerio@gmail.com>
2020-05-08 11:06:30 +0200

    added notification center observer

--- WebViewAccessibility/ViewController.swift
+++ WebViewAccessibility/ViewController.swift
@@ -19,8 +19,33 @@ class ViewController: UIViewController {
             let request = URLRequest(url: URL(fileURLWithPath: bundlePath))
             webView.load(request)
         }
+        NotificationCenter.default.addObserver(self, selector: #selector(preferredContentSizeChanged(_:)), name: UIContentSizeCategory.didChangeNotification, object: nil)
     }
 
     private var webView = WKWebView()
+    private var useCustomFont = true
+    
+    private func notifyFontSize(newSize:CGFloat) {
+        let javascriptFunction = "fontSizeChanged(\(newSize))"
+        webView.evaluateJavaScript(javascriptFunction) { (result, error) in
+            print("font size changed in webview")
+        }
+    }
+    
+    @objc private func preferredContentSizeChanged(_ notification: Notification) {
+        if let userInfo = notification.userInfo,
+            let contentSize = userInfo["UIContentSizeCategoryNewValueKey"] {
+            print("new category \(contentSize) type = \(type(of: contentSize))")
+        }
+        let font = UIFont.preferredFont(forTextStyle: .body)
+        if useCustomFont {
+            notifyFontSize(newSize: font.pointSize)
+        }
+        else {
+            webView.reload()
+        }
+        
+    }
 }
+
 
